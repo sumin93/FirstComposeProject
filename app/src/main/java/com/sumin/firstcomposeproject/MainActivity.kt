32f04059b9bc02ee683ad20787bf768f678a459a
a.sumin <sumin93@mail.ru>
2022-09-07 20:54:47 +0300

    InstagramProfileCard prepare

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -13,12 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sumin.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
+            InstagramProfileCard()
         }
     }
 }
---
+++ app/src/main/java/com/sumin/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -0,0 +1,51 @@
+package com.sumin.firstcomposeproject.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun InstagramProfileCard() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(50.dp)
+                .background(color = Color.Yellow)
+        )
+        TwoBoxes()
+        TwoBoxes()
+        TwoBoxes()
+    }
+}
+
+@Composable
+private fun TwoBoxes() {
+    Column(
+        modifier = Modifier.height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(25.dp)
+                .background(color = Color.Blue)
+        )
+        Box(
+            modifier = Modifier
+                .size(25.dp)
+                .background(color = Color.Red)
+        )
+    }
+}
