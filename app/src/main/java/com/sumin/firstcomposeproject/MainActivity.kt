e87373c8ea30d7df1312f7845fb3c83e5e39922d
a.sumin <sumin93@mail.ru>
2022-09-08 22:02:27 +0300

    Card

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -3,14 +3,20 @@ package com.sumin.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sumin.firstcomposeproject.ui.theme.InstagramProfileCard
@@ -26,34 +32,19 @@ class MainActivity : ComponentActivity() {
 
 @Preview
 @Composable
-fun TimesTable() {
-    Column(
-        modifier = Modifier.fillMaxSize()
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        backgroundColor = Color.Green,
+        contentColor = Color.White,
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        for (i in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                for (j in 1 until 10) {
-                    val color = if ((i + j) % 2 == 0) {
-                        Color.Yellow
-                    } else {
-                        Color.White
-                    }
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, color = Color.DarkGray)
-                            .background(color = color),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "${i * j}")
-                    }
-                }
-            }
-        }
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Hello World!"
+        )
     }
 }
--- app/src/main/java/com/sumin/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,7 +1,10 @@
 package com.sumin.firstcomposeproject.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -12,21 +15,30 @@ import androidx.compose.ui.unit.dp
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        backgroundColor = Color.White,
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        Box(
+        Row(
             modifier = Modifier
-                .size(50.dp)
-                .background(color = Color.Yellow)
-        )
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .background(color = Color.Yellow)
+            )
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
 }
 
