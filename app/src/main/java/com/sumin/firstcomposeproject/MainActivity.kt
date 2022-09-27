3b94590853e288e694ceaac3ab0be7fa243618e7
a.sumin <sumin93@mail.ru>
2022-09-28 00:29:47 +0300

    Example with LazyList

--- app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/sumin/firstcomposeproject/MainActivity.kt
@@ -3,11 +3,17 @@ package com.sumin.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModelProvider
 import com.sumin.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.sumin.firstcomposeproject.ui.theme.InstagramProfileCard
@@ -18,15 +24,41 @@ class MainActivity : ComponentActivity() {
         val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         setContent {
-            FirstComposeProjectTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                ) {
+            Test(viewModel = viewModel)
+        }
+    }
+}
+
+@Composable
+private fun Test(viewModel: MainViewModel) {
+    FirstComposeProjectTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colors.background)
+        ) {
+            LazyColumn {
+                item {
+                    Text(text = "Title", color = Color.White)
+                }
+                items(10) {
+                    InstagramProfileCard(viewModel)
+                }
+                item {
+                    Image(painter = painterResource(id = R.drawable.ic_instagram), contentDescription = null)
+                }
+                items(500) {
                     InstagramProfileCard(viewModel)
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
